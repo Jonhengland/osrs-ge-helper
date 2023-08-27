c0464643f03fa67c24426f12d3124cbf1dba3f53
jengland <jengland@hammerton.com>
2023-08-27 01:19:24 -0700

    Polished themes, added icons for nav bar links, added loose skeleton for certain nav bar items.

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,15 +1,48 @@
 // Navbar.tsx
 
 import React from 'react';
+import geIcon from './icons/ge-icon.png';
+import bossesIcon from './icons/bossesIcon.png';
+import highAlchemyIcon from './icons/highAlchemyIcon.png';
+import searchIcon from './icons/searchIcon.png';
+import skillsIcon from './icons/skillsIcon.png';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><a href="#">All Items</a></li>
-        <li><a href="#">High Alchemy</a></li>
-        <li><a href="#">Boss Setups</a></li>
-        <li><a href="#">Skilling</a></li>
+        <li>
+            <span className='nav-item'>
+                <div className='image-icon-container'>
+                    <img src={searchIcon} alt={"items"} />
+                </div>
+                <a href="#">All Items</a>
+            </span>
+        </li>
+        <li>
+            <span className='nav-item'>
+                <div className='image-icon-container'>
+                    <img src={highAlchemyIcon} alt={"items"} />
+                </div>
+                <a href="#">High Alchemy</a>
+            </span>
+        </li>
+        <li>
+            <span className='nav-item'>
+                <div className='image-icon-container'>
+                    <img src={bossesIcon} alt={"items"} />
+                </div>
+                <a href="#">Boss Setups</a>
+            </span>
+        </li>
+        <li>
+            <span className='nav-item'>
+                <div className='image-icon-container'>
+                    <img src={skillsIcon} alt={"items"} />
+                </div>
+                <a href="#">Skilling</a>
+            </span>
+        </li>
       </ul>
     </nav>
   );
---
+++ src/SearchItems.tsx
@@ -0,0 +1,14 @@
+// SearchItems.tsx
+
+import React from 'react';
+
+function SearchItems() {
+  return (
+    <div>
+      <h1>All Items Page</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+}
+
+export default SearchItems;
